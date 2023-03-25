f603043e03bac51e1738556b31c7a7e313c22dce
dunxd <d.drury@gmail.com>
2023-03-25 18:56:39 +0000

    All references to mail servers removed - working with no mail function

--- COPS/run.sh
+++ COPS/run.sh
@@ -4,12 +4,6 @@
 #Capture Options set by user
 TITLE=$(bashio::config 'title')
 TIMEZONE=$(bashio::config 'timezone')
-MAIL_HOST=$(bashio::config 'mail_host')
-MAIL_FROM_ADDRESS=$(bashio::config 'mail_from_address')
-MAIL_USERNAME=$(bashio::config 'mail_username')
-MAIL_PASSWORD=$(bashio::config 'mail_password')
-MAIL_SECURE=$(bashio::config 'mail_secure')
-
 
 COPS_CONFIG="/cops/config_local.php"
 
@@ -20,31 +14,7 @@ cat << END_HEREDOC
 END_HEREDOC
 )
 
-if [[ -n $MAIL_HOST ]]
-then
-CONFIG_STR+=$(
-cat << END_HEREDOC2
-\$config['cops_mail_configuration'] = array( 'smtp.host'     => '$MAIL_HOST',
-                                             'smtp.username' => '$MAIL_USERNAME',
-                                             'smtp.password' => '$MAIL_PASSWORD',
-                                             'address.from'  => '$MAIL_FROM_ADDRESS'
-                                            );
-END_HEREDOC2
-)
-fi
-
-if $MAIL_SECURE
-then
-CONFIG_STR+=$(
-cat << END_HEREDOC3
-\$config['cops_mail_configuration'] += ['smtp.secure' => 'ssl'];
-END_HEREDOC3
-)
-fi
-
 echo "$CONFIG_STR" >> "$COPS_CONFIG"
-bashio::log.info "$(cat /cops/config_local.php)"
-
 
 mkdir -p /media/books
 cd /cops || return
