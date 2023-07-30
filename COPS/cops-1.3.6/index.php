5cc83f2eb5a2aa81d78ebf7a7e3f97326b6a2f6b
Duncan Drury <d.drury@gmail.com>
2023-07-31 00:06:58 +0100

    Detect Librera reader and redirect to feed.php

--- COPS/cops-1.3.6/index.php
+++ COPS/cops-1.3.6/index.php
@@ -14,7 +14,7 @@ require_once dirname(__FILE__) . '/base.php';
 /** @var array $config */
 
 // If we detect that an OPDS reader try to connect try to redirect to feed.php
-if (preg_match('/(MantanoReader|FBReader|Stanza|Marvin|Aldiko|Moon\+ Reader|Chunky|AlReader|EBookDroid|BookReader|CoolReader|PageTurner|books\.ebook\.pdf\.reader|com\.hiwapps\.ebookreader|OpenBook)/', $_SERVER['HTTP_USER_AGENT'])) {
+if (preg_match('/(Librera|MantanoReader|FBReader|Stanza|Marvin|Aldiko|Moon\+ Reader|Chunky|AlReader|EBookDroid|BookReader|CoolReader|PageTurner|books\.ebook\.pdf\.reader|com\.hiwapps\.ebookreader|OpenBook)/', $_SERVER['HTTP_USER_AGENT'])) {
     header('location: feed.php');
     exit();
 }
