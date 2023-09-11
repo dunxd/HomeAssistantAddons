613d77aa37579848e2d2203441a99765b83458ee
dunxd <d.drury@gmail.com>
2023-09-11 12:15:35 +0000

    Moved initiateTwig to cops.js in templates/twigged/scripts

--- COPS/cops-2.0.1/util.js
+++ COPS/cops-2.0.1/util.js
@@ -538,29 +538,3 @@ function initiateAjax (url, theme) {
         handleLinks ();
     });
 }
-
-function initiateTwig(url, theme) {
-    Twig.extendFunction("str_format", str_format);
-    Twig.extendFunction("asset", asset);
-
-    let template = Twig.twig({
-        id: 'page',
-        href: 'templates/' + theme + '/page.html',
-        async: false 
-     });
-
-     templatePage = function (data) {
-        return Twig.twig({ref: 'page'}).render({it: data});
-     };
-
-     $.when($.getJSON(url)).done(function(data){
-        currentData = data;
-
-        updatePage (currentData);
-        cache.put (url, currentData);
-        if (isPushStateEnabled) {
-            window.history.replaceState(url, "", window.location);
-        }
-        handleLinks ();
-    });
-}
