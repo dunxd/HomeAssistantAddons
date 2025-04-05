d9cd3e362a77bc42547882164f2b96d1507f5bea
dunxd <d.drury@gmail.com>
2025-04-05 15:59:30 +0000

    Required changes to COPS config

--- COPS/cops-3.5.7/config/local.php
+++ COPS/cops-3.5.7/config/local.php
@@ -105,4 +105,6 @@ $config['cops_home_page'] = 'ALL_RECENT_BOOKS';
  */
 $config['cops_front_controller'] = 'router.php';
 
+/* Add kepubify path to support converting kepub files */
 $config['cops_kepubify_path'] = '/usr/bin/kepubify';
+$config['cops_provide_kepub'] = '1';
