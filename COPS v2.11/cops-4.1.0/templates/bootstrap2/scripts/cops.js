7bcf0e9166197b77fcad7968d9b4ec4428b54920
dunxd <d.drury@gmail.com>
2026-05-03 08:43:31 +0000

    Remove alternative versions

--- COPS v2.11/cops-4.1.0/templates/bootstrap2/scripts/cops.js
+++
@@ -1,7 +0,0 @@
-function postRefresh()
-{
-    $('[data-toggle="tooltip"]').tooltip();
-    hash = window.location.hash.replace("#", "");
-    var elmnt = document.getElementById(hash);
-    if (elmnt) elmnt.scrollIntoView();
-}
